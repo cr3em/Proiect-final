b1927fe35bc45a208d553968eee11b9a1a1eaa3d
Emil Cretan <55461317+cr3em@users.noreply.github.com>
2020-02-07 14:18:41 +0200

    Cinematograful
    revizia 0.2
    
    creeat Meniu de inceput

--- Cinematograful/main.cpp
+++ Cinematograful/main.cpp
@@ -2,9 +2,28 @@
 
 using namespace std;
 
+void Meniu() {
+    system("cls");
+    cout << "1. Vinde bilete\n";
+    cout << "2. Returneaza bilete\n";
+    cout << "3. Afiseaza situatie locuri libere\n";
+    cout << "4. Modifica preturile\n";
+    cout << "0. Iesire\n";
+}
+
+
+
 int main()
 {
+    unsigned short opt = 1;
+    while (opt != 0) {
+        while (opt > 4) {
+            cout << "Optiune gresita, introduceti din nou\n";
+            cin >> opt;
+        }
 
+
+    }
 
 
     return 0;
